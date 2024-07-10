ca222743b608ff86560b78f75c3e788b973b2bfe
JoseDev <132334488+devjldp@users.noreply.github.com>
2024-07-10 20:16:08 +0100

    day 1

---
+++ Variables/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Hello World!");
+
+Console.WriteLine("My name is Jose");
+
+/* To open a new terminal => ctrl + '
+ *  To run the script dotnet run
+ */
+
+/*
+ * using System; => allows the code to use classes and 
+ *                  other types defined in the System namespace without needing to fully qualify their names. 
+ * 
+ * namespace consoleapp2 => defines a namespace named consoleapp2. Namespaces are used to organize code 
+ *                          and prevent naming conflicts by grouping related classes, interfaces, and functions together.
+ * {
+ *      internal class Program =>  defines a class named Program with an access modifier internal.
+ *      {
+ *          static void Main(string[] args)
+ *          {
+ *              console.writeline("hello world!");
+ *          }
+ *      }
+ * }
+ */
+
+// Creating my first variable
+
+// string fullName = 'Jose'; No => ' ' is used for characters
+string fullName = "Jose"; // OK
+
+Console.WriteLine("Hello" + fullName); // No space
+
+Console.WriteLine("Hello " + fullName); // Space after Hello
+
+// Using string interpolation
+Console.WriteLine($"Hello {fullName}");
+
+
+// Interacting with terminal
+
+Console.WriteLine("What is your name?");
+fullName = Console.ReadLine();
+// We have a warning => we can use fullName = Console.ReadLine() ?? "Default Name"; to solve it
+Console.WriteLine($"Hello {fullName}");
---
+++ week1.md
@@ -0,0 +1,16 @@
+# Week 1
+
+## Day 1
+
+- Printed messages to the console.
+- Learned to open a new terminal in Visual Studio (`Ctrl + '`).
+- Ran scripts using `dotnet run` in the terminal.
+- Explained using `using System;` for namespace simplification.
+- Created variables (`string fullName = "Jose";`).
+- Demonstrated console output techniques:
+  - Concatenation (`Console.WriteLine("Hello " + fullName);`)
+  - String interpolation (`Console.WriteLine($"Hello {fullName}");`).
+- Interacted with the terminal by prompting and reading user input (`Console.ReadLine()`).
+  
+---
+
